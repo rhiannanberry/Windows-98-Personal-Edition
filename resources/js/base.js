7ab023af5385c07946e171d07dae3aedbf8e5106
Rhiannan Berry <rhiannanb97@gmail.com>
2017-09-01 16:09:57 -0400

    Added copying and better buttons!

--- resources/js/base.js
+++ resources/js/base.js
@@ -4,4 +4,15 @@ $(document).ready(function() {
 		$(this).toggleClass('active');
 		$('.start-menu').toggleClass('open');
 	});
+
+//TODO: Create popup for successful copy and also add fail safes
+	$('#email').on('click', function(e) {
+		var txt = document.createElement('textarea');
+		txt.textContent = "rberry36@gatech.edu";
+		txt.style.position = 'fixed';
+		document.body.appendChild(txt);
+		txt.select();
+		document.execCommand('copy');
+		document.body.removeChild(txt);
+	});
 });
